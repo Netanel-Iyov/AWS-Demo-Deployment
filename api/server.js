723f82d5212ae9318f1a99d01c17a26a66cc012e
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-07-23 13:53:25 +0300

    adding api and communication between front and api

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors());
+
+const corsOptions = {
+  origin: [
+    "http://18.203.100.180",
+    "http://aws-deployment-demo.niyov.com",
+    "http://127.0.0.1:5500",
+  ],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+let counter = 10;
+
+// Sample GET route with parameters
+app.get("/counter", (req, res) => {
+  res.json({ counter: counter });
+});
+
+// Sample POST route
+app.post("/counter/increase", (req, res) => {
+  counter += 1;
+  res.json({ counter: counter });
+});
+
+// Sample POST route
+app.post("/counter/decrease", (req, res) => {
+  counter -= 1;
+  res.json({ counter: counter });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ front/index.js
@@ -0,0 +1,50 @@
+const apiBaseUrl = "http://localhost:3000";
+
+DisplayInitialCounterValue();
+
+async function increment() {
+  const apiUrl = `${apiBaseUrl}/counter/increase`;
+  response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  document.getElementById("counter").innerText = data.counter;
+}
+
+async function decrement() {
+  const apiUrl = `${apiBaseUrl}/counter/decrease`;
+  response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  document.getElementById("counter").innerText = data.counter;
+}
+
+async function DisplayInitialCounterValue() {
+  const apiUrl = `${apiBaseUrl}/counter`;
+  response = await fetch(apiUrl);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  document.getElementById("counter").innerText = data.counter;
+}
