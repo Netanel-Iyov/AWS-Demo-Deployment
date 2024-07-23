f2e6232061ce5de03fbe44b08ffe3e9e20bb359d
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-07-23 15:22:10 +0300

    changing api ip to be EC2 public IP

--- api/server.js
+++ api/server.js
@@ -40,5 +40,5 @@ app.post("/counter/decrease", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at port: ${port}`);
 });
